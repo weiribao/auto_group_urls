b7f899a2b7d00c6e36513b986e7bcf5d94feb986
rwei <rwei@rwei-E6420.(none)>
2012-10-20 12:44:05 -0400

    add html form

--- app.js
+++ app.js
@@ -8,12 +8,25 @@ var group = require("./group.js");
 app.use(express.bodyParser());
 
 app.get("/", function(req, res){
-	res.send("Usage: post / {urls: [url1, url2, ...]}\n");
+	res.send("Usage: post / {urls: [url1, url2, ...]}<form action='/' method='post'><textarea name='urlstr' rows='30' cols='80'></textarea><br><input type='submit' value='submit'></form>");
 })
 app.post("/", function(req, res){
-	// console.log(req.body);
-	var doc = group.printXml(group.process(req.body.urls));
+	console.log(req.body);
+	var urls = req.body.urls || parseSource(req.body.urlstr) || [];
+	console.log(urls);
+	var doc = group.printXml(group.process(urls));
 	res.send(doc);
 });
 
 app.listen(8001);
+
+function parseSource(raw){
+	var source = raw
+			.trim()
+			.replace("\r", "")
+			.split(/[\r\n]+/)
+			.filter(function(line){
+				return line.trim()!="";
+			});
+	return source;
+}
